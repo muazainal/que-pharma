520a9a94b8b40ed2ad42bb1f550315ed5a6a69c5
muazainal <muazainal@live.com>
2026-03-15 17:18:31 +0000

    refactor: simplify `ModelAdmin` inheritance in admin classes.

--- queue_manager/admin.py
+++ queue_manager/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from .models import PharmacistProfile, PrescriptionTicket
 
 @admin.register(PrescriptionTicket)
-class PrescriptionTicketAdmin(admin.admin.ModelAdmin):
+class PrescriptionTicketAdmin(admin.ModelAdmin):
     list_display = ('queue_number', 'patient_name', 'status', 'created_at')
     list_filter = ('status', 'created_at')
     search_fields = ('patient_name', 'queue_number')
 
 @admin.register(PharmacistProfile)
-class PharmacistProfileAdmin(admin.admin.ModelAdmin):
+class PharmacistProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'employee_id')
     search_fields = ('user__username', 'employee_id')
